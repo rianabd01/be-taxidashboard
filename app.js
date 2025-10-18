8c7653deb52206c8080bd70f914f0fedac6be6f1
rianabdillah <44765565+rianabd01@users.noreply.github.com>
2025-10-18 22:53:27 +0700

    fix

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const cors = require("cors");
 
 app.use(
